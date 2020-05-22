d639c50175daaed0d184c8588d5e6eb4cb3abe4d
Nikolay Amiantov <ab@fmap.me>
2020-05-22 19:08:51 +0300

    Fix some more

--- tasks/mybrick/app/start_server.sh
+++ tasks/mybrick/app/start_server.sh
@@ -11,10 +11,15 @@ fi
 [[ CREATE -eq 1 ]] && mkdir $1/mongo
 mongod --auth --dbpath $1/mongo --bind_ip $1/mongo.sock --logpath $1/mongo-logs &
 if [[ CREATE -eq 1 ]]; then
-  while [ ! -f $1/mongo.sock ]; do
-    sleep 1
-  done
-  mongo --host $1/mongo.sock admin --eval "db.createUser({user: \"$MONGO_USER\", pwd: \"$MONGO_PASS\", roles: [{role: \"userAdminAnyDatabase\", db: \"admin\"}]})"
+    for i in seq 1 5; do
+        if mongo --host $1/mongo.sock admin --eval "db.createUser({user: \"$MONGO_USER\", pwd: \"$MONGO_PASS\", roles: [{role: \"userAdminAnyDatabase\", db: \"admin\"}]})"; then
+            DONE=1
+            break
+        fi
+    done
+    if [[ CREATE -ne 1 ]]; then
+        exit 1
+    fi
 fi
 
 ./server.py $1
