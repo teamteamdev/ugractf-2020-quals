4de6a2d242fdebd7add49301b662959dcc3a88a0
Nikolay Amiantov <ab@fmap.me>
2020-05-22 19:05:36 +0300

    Fix mongo race condition

--- tasks/mybrick/app/start_server.sh
+++ tasks/mybrick/app/start_server.sh
@@ -10,6 +10,11 @@ fi
 
 [[ CREATE -eq 1 ]] && mkdir $1/mongo
 mongod --auth --dbpath $1/mongo --bind_ip $1/mongo.sock --logpath $1/mongo-logs &
-[[ CREATE -eq 1 ]] && mongo --host $1/mongo.sock admin --eval "db.createUser({user: \"$MONGO_USER\", pwd: \"$MONGO_PASS\", roles: [{role: \"userAdminAnyDatabase\", db: \"admin\"}]})"
+if [[ CREATE -eq 1 ]]; then
+  while [ ! -f $1/mongo.sock ]; do
+    sleep 1
+  done
+  mongo --host $1/mongo.sock admin --eval "db.createUser({user: \"$MONGO_USER\", pwd: \"$MONGO_PASS\", roles: [{role: \"userAdminAnyDatabase\", db: \"admin\"}]})"
+fi
 
 ./server.py $1
