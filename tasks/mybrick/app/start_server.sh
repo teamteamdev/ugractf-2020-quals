00740b1b1a40610b45af30c6efbab3b7aa7d1080
Nikita Sychev <root@nsychev.ru>
2020-05-22 23:30:42 +0500

    mybrick: i hate mongodb

--- tasks/mybrick/app/start_server.sh
+++ tasks/mybrick/app/start_server.sh
@@ -5,22 +5,25 @@ export MONGO_USER=root
 export MONGO_PASS=mybricksecretpass111
 
 if [[ ! -d $1/mongo ]]; then
-    CREATE=1
-fi
-
-[[ CREATE -eq 1 ]] && mkdir $1/mongo
-mongod --auth --dbpath $1/mongo --bind_ip $1/mongo.sock --logpath $1/mongo-logs &
-if [[ CREATE -eq 1 ]]; then
+    mongod --dbpath $1/mongo --bind_ip $1/mongo.sock --logpath $1/mongo-logs &
     for i in seq 1 5; do
-        if mongo --host $1/mongo.sock admin --eval "db.createUser({user: \"$MONGO_USER\", pwd: \"$MONGO_PASS\", roles: [{role: \"userAdminAnyDatabase\", db: \"admin\"}]})"; then
+        sleep 1
+        if mongo --host $1/mongo.sock db --authenticationDatabase admin --eval "db.createUser({user: \"$MONGO_USER\", pwd: \"$MONGO_PASS\", roles: [{role: \"userAdminAnyDatabase\", db: \"db\"}]})"; then
             DONE=1
             break
         fi
-        sleep 1
     done
-    if [[ CREATE -ne 1 ]]; then
+
+    if [[ DONE -ne 1 ]]; then
+        echo "Database creation failed."
         exit 1
     fi
+
+    mongo --host $1/mongo.sock db --eval "db.adminCommand( { shutdown: 1 } )"
+    wait
 fi
+
+mongod --auth --dbpath $1/mongo --bind_ip $1/mongo.sock --logpath $1/mongo-logs &
+sleep 5
 
 ./server.py $1
