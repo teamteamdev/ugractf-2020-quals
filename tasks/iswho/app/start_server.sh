36b3dec3e5a8d6573a75dbcf5dbae7da14f0b10e
Nikolay Amiantov <ab@fmap.me>
2020-05-23 15:39:13 +0300

    Stop iswho gracefully

--- tasks/iswho/app/start_server.sh
+++ tasks/iswho/app/start_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 export SOCK_PATH=$1/sock
-exec docker-compose up --build
+exec docker-compose -p "$NEW_UUID" up
