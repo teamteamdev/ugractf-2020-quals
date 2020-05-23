3839ff366d2b7362acfc704d65197102147fb1b3
Nikolay Amiantov <ab@fmap.me>
2020-05-23 07:45:17 +0300

    Fix socket, maybe this time

--- tasks/iswho/app/run_internal.sh
+++ tasks/iswho/app/run_internal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -f /iswho/sock/iswho.sock
-socat UNIX-LISTEN:/iswho/sock/iswho.sock,perm-early=0666,fork TCP4-CONNECT:3000 &
+socat UNIX-LISTEN:/iswho/sock/iswho.sock,mode=0666,fork TCP4-CONNECT:3000 &
 java -jar /iswho/iswho.jar
